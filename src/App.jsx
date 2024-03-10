ec47624636bd0f8306577a6b603ec3a0bd6cd344
Joy <github.joysaha@gmail.com>
2024-03-10 10:39:17 +0600

    create demo project for practice event

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import './App.css'
 
 function App() {
   const purple = "#8e44ad";
-  const [bg, setBg] = useState(purple)
+  const [bg, setBg] = useState(purple);
+  const [name, setName] = useState('click me');
 
   const bgChange = () => {
     // console.log('clicked')
     let newBg = '#34495e'
     setBg(newBg)
+    setName("Ouch!! : 😲")
+  }
+
+  const bgBack = () => {
+    setBg(purple);
+    setName("Ayyo!! : 😎")
   }
 
   return (
     <>
       <div style={ {backgroundColor: bg}}>
-        <button onClick={bgChange}>Button</button>
+        <button onMouseEnter={bgChange} onMouseLeave={bgBack}>{name}</button>
       </div>
       
       
